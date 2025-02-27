4b24ef0d90f5a783644691fa585df696605bda9b
inverse9 <permanawahyu97@gmail.com>
2025-02-27 16:50:23 +0800

    add floating button & new layout for add new alarm with the toolbar

--- app/src/main/java/com/example/alarmapp/AddAlarm.java
+++ app/src/main/java/com/example/alarmapp/AddAlarm.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -17,31 +18,18 @@ public class AddAlarm extends AppCompatActivity {
         setContentView(R.layout.activity_add_alarm);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
 
-        // Enable back button
-        if (getSupportActionBar() != null) {
-            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        }
+        ImageView btnBack = findViewById(R.id.btn_back);
+        ImageView btnCheck = findViewById(R.id.btn_check);
 
-        toolbar.setNavigationOnClickListener(v -> finish()); // Back button action
+        btnBack.setOnClickListener(v -> finish());
+
+        btnCheck.setOnClickListener(v -> {
+            Toast.makeText(AddAlarm.this, "Alarm Saved!", Toast.LENGTH_SHORT).show();
+        });
     }
 
-    // Inflate the menu (adds the check button)
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.add_alarm, menu);
-        return true;
-    }
 
-    // Handle menu item clicks
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if (item.getItemId() == R.id.action_save) {
-            Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 }
 
